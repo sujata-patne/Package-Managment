1318390b512bc6237e09148cd140ee5ff77ede6b
nishtha-gupta <nishta1992@gmail.com>
2015-09-30 16:46:42 +0530

    changes

--- public/scripts/services/mainSite.service.js
+++ public/scripts/services/mainSite.service.js
@@ -2,13 +2,13 @@
  * Created by sujata.patne on 29-09-2015.
  */
 myApp.service('MainSite', ['$http', function ($http) {
-    var service = {};
-    service.baseRestUrl = 'http://localhost:3080';
-    service.getContentTypes = function(success){
-        $http.get(service.baseRestUrl + '/getContentTypes').success(function (items) {
+    //var service = {};
+    this.baseRestUrl = 'http://localhost:3080';
+    this.getContentTypes = function(success){
+        $http.get(this.baseRestUrl + '/getContentTypes').success(function (items) {
             console.log(items)
             success(items);
         });
     }
-    return service;
+    
 }]);
--- public/scripts/services/users.service.js
+++ public/scripts/services/users.service.js
@@ -2,36 +2,36 @@
  * Created by sujata.patne on 14-07-2015.
  */
 myApp.service('Users', ['$http', function ($http) {
-    var service = {};
-    service.baseRestUrl = 'http://localhost:3000';
-    service.getUsers = function(success){
-        $http.get(service.baseRestUrl + '/users').success(function (items) {
+    //var service = {};
+    this.baseRestUrl = 'http://localhost:3000';
+    this.getUsers = function(success){
+        $http.get(this.baseRestUrl + '/users').success(function (items) {
             success(items);
         });
     }
 
-    service.updateUser = function(data,success){
-        $http.post(service.baseRestUrl + '/updateUsers', data).success(function (items) {
+    this.updateUser = function(data,success){
+        $http.post(this.baseRestUrl + '/updateUsers', data).success(function (items) {
             success(items);
         });
     }
-    service.saveUser = function(data,success){
-        $http.post(service.baseRestUrl + '/saveUsers', data).success(function (items) {
-            success(items);
-        });
-    }
-
-    service.addEditUsers = function(data,success){
-        $http.post(service.baseRestUrl + '/addEditUsers',data).success(function (items) {
+    this.saveUser = function(data,success){
+        $http.post(this.baseRestUrl + '/saveUsers', data).success(function (items) {
             success(items);
         });
     }
 
-    service.changePassword = function(data,success){
-        $http.post(service.baseRestUrl + '/changepassword',data).success(function (items) {
+    this.addEditUsers = function(data,success){
+        $http.post(this.baseRestUrl + '/addEditUsers',data).success(function (items) {
             success(items);
         });
     }
 
-    return service;
+    this.changePassword = function(data,success){
+        $http.post(this.baseRestUrl + '/changepassword',data).success(function (items) {
+            success(items);
+        });
+    }
+
+    //return service;
 }]);
