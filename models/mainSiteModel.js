f2adebfdfffe5a401f19816854d3cada40531ecd
pratikm23 <pratikm23@gmail.com>
2015-10-09 12:46:48 +0530

    validation for undefined package id

--- models/mainSiteModel.js
+++ models/mainSiteModel.js
@@ -123,6 +123,9 @@ exports.existStorePackage = function(dbConnection,storeId, dcId, callback) {
 }
 
 exports.getPackageOfferPlan = function( dbConnection, packageId, callback ) {
+    if(packageId == undefined){
+        packageId = -1;
+    }
     var query = dbConnection.query('SELECT * FROM `icn_package_alacart_offer_site`,`icn_offer_plan` WHERE '+
         ' icn_package_alacart_offer_site.paos_sp_pkg_id = ? AND '+
         ' icn_package_alacart_offer_site.paos_op_id = icn_offer_plan.op_id',packageId, function( err, response ) {
@@ -131,6 +134,9 @@ exports.getPackageOfferPlan = function( dbConnection, packageId, callback ) {
 }
 
 exports.getPackageValuePack = function( dbConnection, packageId, callback ) {
+    if(packageId == undefined){
+        packageId = -1;
+    }
     var query = dbConnection.query('SELECT * FROM `icn_package_value_pack_site`,`icn_valuepack_plan` WHERE '+
         'icn_package_value_pack_site.pvs_sp_pkg_id = ? AND '+
         'icn_package_value_pack_site.pvs_vp_id = icn_valuepack_plan.vp_id',packageId, function( err, response ) {
