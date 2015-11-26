6b4b1fd8ee8bc18295667cbbb6b7715883b17c32
pratikm23 <pratikm23@gmail.com>
2015-11-26 15:47:16 +0530

    thumbnail changes

--- models/individualContentModel.js
+++ models/individualContentModel.js
@@ -12,7 +12,7 @@ exports.getLastInsertedIndividualContentId = function( dbConnection, callback) {
 
 
 exports.getContentData = function( dbConnection,contentTypeId,packId, callback) {
-	var query = dbConnection.query("SELECT *,(SELECT `catalogue_detail`.cd_name   FROM `icn_manage_content_type`,`catalogue_detail` where mct_cnt_type_id = content_metadata.cm_content_type and mct_parent_cnt_type_id = catalogue_detail.cd_id) as parent_type_name,(SELECT cf.cf_url FROM content_files as cf WHERE content_metadata.cm_id = cf.cf_cm_id Limit 1) AS contentUrl  FROM icn_pack_content,content_metadata WHERE pc_pct_id = (SELECT pct_id from icn_pack_content_type where pct_cnt_type = ? AND pct_pk_id = ?) AND ISNULL(pc_crud_isactive) AND content_metadata.cm_id = icn_pack_content.pc_cm_id",[contentTypeId,packId], function ( err, response ) {
+	var query = dbConnection.query("SELECT *,(SELECT `catalogue_detail`.cd_name   FROM `icn_manage_content_type`,`catalogue_detail` where mct_cnt_type_id = content_metadata.cm_content_type and mct_parent_cnt_type_id = catalogue_detail.cd_id) as parent_type_name,(SELECT cf.cf_url FROM content_files as cf WHERE content_metadata.cm_id = cf.cf_cm_id Limit 1) AS contentUrl ,(SELECT cft_thumbnail_img_browse FROM content_files_thumbnail WHERE cft_cm_id = icn_pack_content.pc_cm_id) as new_thumb_url FROM icn_pack_content,content_metadata WHERE pc_pct_id = (SELECT pct_id from icn_pack_content_type where pct_cnt_type = ? AND pct_pk_id = ?) AND ISNULL(pc_crud_isactive) AND content_metadata.cm_id = icn_pack_content.pc_cm_id",[contentTypeId,packId], function ( err, response ) {
 		callback( err, response );
 	});
 }
